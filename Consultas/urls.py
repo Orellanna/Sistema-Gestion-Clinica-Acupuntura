afb31095717f9ac3bcf8fac277f3e1c63922d0d0
Alexis Orellana <od18003@ues.edu.sv>
2023-05-14 20:32:08 -0600

    fix. Historial Consulta

--- Consultas/urls.py
+++ Consultas/urls.py
@@ -5,7 +5,8 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('HistorialConsultas/', views.ListarConsultas, name='ListarConsultas'),
-    path('NuevaConsulta/', views.NuevaConsulta, name='NuevaConsulta'),
+    path('<int:paciente_id>/HistorialConsultas/', views.ListarConsultas, name='ListarConsultas'),
+    path('<int:paciente_id>/HistorialConsultas/NuevaConsulta/', views.NuevaConsulta, name='NuevaConsulta'),
+    #path('NuevaConsulta/<int:paciente_id>/', views.NuevaConsulta, name='NuevaConsulta'),
     path('DetallesConsulta/<int:consulta_id>/', views.DetallesConsulta, name='DetallesConsulta'),
 ]
--- Consultas/views.py
+++ Consultas/views.py
@@ -1,17 +1,47 @@
 # Create your views here.
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from django.http import HttpResponse
 from Pacientes.models import Consulta, Paciente
-
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return render(request,'index.html')
 
-def ListarConsultas(request):
-    return render(request,'HistorialConsultas.html')
+def ListarConsultas(request, paciente_id):
+    paciente = get_object_or_404(Paciente, id_paciente =paciente_id)
+    consultas = Consulta.objects.filter(id_paciente=paciente_id)
+    return render(request,'HistorialConsultas.html',{
+        'consultas': consultas, 
+        'paciente': paciente,
+    })
 
-def NuevaConsulta(request):
-    return render(request,'NuevaConsulta.html')
+@csrf_exempt
+def NuevaConsulta(request, paciente_id):
+    
+    paciente = get_object_or_404(Paciente, id_paciente=paciente_id)
+    
+    if request.method == 'POST':
+        
+        #obtenemos los datos del formulario
+        fecha_consulta = request.POST['fecha_consulta']
+        motivo = request.POST['motivo']
+        obervacion_consulta = request.POST['obervacion_consulta']
+        finalizada = request.POST.get('finalizada', False) == 'on'
+        
+        #Creamos la nueva consulta para el paciente
+        consulta = Consulta.objects.create(
+            id_paciente = paciente,
+            consulta_fecha = fecha_consulta,
+            motivo = motivo,
+            obervacion_consulta = obervacion_consulta,
+            finalizada = finalizada,
+        )
+        
+        #Redirige al usuario a la pagina del historial de consultas del paciente 
+        return redirect('ListarConsultas', paciente_id=paciente_id)
+    
+    return render(request,'NuevaConsulta.html',{'paciente': paciente})
+           
 
 def DetallesConsulta(request, consulta_id):
     consulta = get_object_or_404(Consulta, pk=consulta_id)
