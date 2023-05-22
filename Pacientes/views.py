0b709d8d9ee1f55f54960739296b242ecace5744
jcmracing3116 <mr09090@ues.edu.sv>
2023-05-21 20:13:52 -0600

    mostrar pacientes

--- Pacientes/views.py
+++ Pacientes/views.py
@@ -13,13 +13,68 @@ def index(request):
 
 @login_required
 def GestionPacientes(request):
+
+    paciente = Paciente.objects.all()
+
     return render(request,'Vistas_Pacientes/GestionPacientes.html')
 
 @login_required
 def DetallesConsulta(request):
     return render(request,'Vistas_Pacientes/DetallesConsulta.html')
 
+@login_required
+def EditarPaciente(request, id_paciente, primer_nombre, segundo_nombre, primer_apellido , segundo_apellido, edad_paciente, sexo_paciente, telefono_paciente, email_paciente):
+
+    
+    Paciente = get_object_or_404(Paciente, primer_nombre=primer_nombre)
+    Paciente = get_object_or_404(Paciente, segundo_nombre=segundo_nombre)
+    Paciente = get_object_or_404(Paciente, primer_apellido=primer_apellido)
+    Paciente = get_object_or_404(Paciente, segundo_apellido=segundo_apellido)
+    Paciente = get_object_or_404(Paciente, edad_paciente=edad_paciente)
+    Paciente = get_object_or_404(Paciente, sexo_paciente=sexo_paciente)
+    Paciente = get_object_or_404(Paciente, telefono_paciente=telefono_paciente)
+    Paciente = get_object_or_404(Paciente, email_paciente=email_paciente)
+
+    
+    if request.method == 'POST':
+
+        primer_nombre     = request.POST['InputPrimerNombre']
+        segundo_nombre    = request.POST['InputSegundoNombre']
+        primer_apellido   = request.POST['InputPrimerApellido']
+        segundo_apellido  = request.POST['InputSegundoApellido']
+        edad_paciente     = request.POST['InputEdad'] 
+        sexo_paciente     = request.POST['SelectSexo']
+        telefono_paciente = request.POST['InputTelefono']
+        email_paciente    = request.POST['InputCorreo']
+ 
+        # modificacion de los campos del paciente
+        Paciente = Paciente.objects.get(
+            
+            primer_nombre     = primer_nombre,
+            segundo_nombre    = segundo_nombre,
+            primer_apellido   = primer_apellido,
+            segundo_apellido  = segundo_apellido,
+            edad_paciente     = edad_paciente,
+            sexo_paciente     = sexo_paciente,
+            telefono_paciente = telefono_paciente,
+            email_paciente    = email_paciente,
+        )
+        # Generamos la URL correcta usando reverse
+        url = reverse('GestionPacientes', kwargs={'paciente_id': id_paciente})
+        
+        # Redirigimos al usuario a la URL correcta
+        return redirect(url)
+
+
+
+
+    return render(request,'Vistas_Pacientes/EditarPaciente.html')
+
+
 def DatosPersonales(request):
+
+
+
     return render(request,'Vistas_Pacientes/DatosPersonales.html')
 
 @login_required
@@ -59,6 +114,11 @@ def Registrar(request, id_paciente, primer_nombre, segundo_nombre, primer_apelli
             telefono_paciente = telefono_paciente,
             email_paciente    = email_paciente,
         )
+        # Generamos la URL correcta usando reverse
+        url = reverse('GestionPacientes', kwargs={'paciente_id': id_paciente})
+        
+        # Redirigimos al usuario a la URL correcta
+        return redirect(url)
             
      
     return render(request,'Vistas_Pacientes/RegistrarPaciente.html')
