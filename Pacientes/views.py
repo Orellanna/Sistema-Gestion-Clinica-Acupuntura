790124f8a48d3da9018c81fcf5d7d107758c7249
LF18010 <lf18010@ues.edu.sv>
2023-05-26 15:58:09 -0600

    backend detalles paciente

--- Pacientes/views.py
+++ Pacientes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from datetime import date, datetime
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from .models import Paciente
 @login_required
 def index(request):
     return render(request,'index.html')
@@ -17,8 +18,21 @@ def DetallesConsulta(request):
 
 
 @login_required
-def DatosPersonales(request):
-    return render(request,'Vistas_Pacientes/DatosPersonales.html')
+def DatosPersonales(request, id_paciente):
+    paciente = get_object_or_404(Paciente, id_paciente=id_paciente)
+    fechanac_paciente = paciente.fechanac_paciente
+    edad = CalcularEdad(fechanac_paciente)
+    return render(request,'Vistas_Pacientes/DatosPersonales.html', {
+        'paciente': paciente,
+        'edad' : edad,
+    })
+    
+def CalcularEdad(fechanac_paciente):
+    hoy = date.today()
+    edad = hoy.year - fechanac_paciente.year
+    if hoy.month < fechanac_paciente.month or (hoy.month == fechanac_paciente.month and hoy.day < fechanac_paciente.day):
+        edad -=1
+    return edad
 
 @login_required
 def Registrar(request):
