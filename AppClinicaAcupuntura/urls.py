e0e69b4ef19e27d0e327095da6def36b329fc742
Alexis Orellana <od18003@ues.edu.sv>
2023-08-01 10:49:04 -0600

    add Terapias

--- AppClinicaAcupuntura/urls.py
+++ AppClinicaAcupuntura/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import include
 from Pacientes import views
 from Consultas import views
 from Cuentas import views
+from Terapias import views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -15,5 +16,6 @@ urlpatterns = [
     path('', include('Consultas.urls')),
     path('',include('Cuentas.urls')),
     path('',include('Inventario.urls')),
+    path('',include('Terapias.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
