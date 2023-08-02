5bb59f3e6743cb17e90f8ec340eccc5aa382543d
LF18010 <lf18010@ues.edu.sv>
2023-08-02 16:17:00 -0600

    inc. nuevoPago

--- Pagos/urls.py
+++ Pagos/urls.py
@@ -8,8 +8,9 @@ from Pagos.views import NuevoPago
 
 
 urlpatterns = [
-    path('NuevoPago/', views.NuevoPago, name='NuevoPago'),
+    path('NuevoPago/<str:paciente_id>/', views.NuevoPago, name='NuevoPago'),
     path('HistorialPagos/<str:paciente_id>/', views.ListarPagos, name='ListarPagos'),
-    path('HistorialPagos/<str:paciente_id>/DetallesPago/<str:pago_id>/', views.DetallesPago, name='DetallesPago'),
+    path('HistorialPagos/<str:paciente_id>/<str:consulta_id>/DetallesPago/<int:pago_id>/', views.DetallesPago, name='DetallesPago'),
+    path('HistorialPagos/<str:paciente_id>/EditarPago/<str:pago_id>/', views.EditarPago, name='EditarPago'),
 
     ]
