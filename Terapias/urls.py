49cb7e06e60aa7e7958cc3c7bb4fa32ae845aaf7
Alexis Orellana <od18003@ues.edu.sv>
2023-08-03 01:11:58 -0600

    Modulos Nueva Terapia & Historial Terapias

--- Terapias/urls.py
+++ Terapias/urls.py
@@ -5,9 +5,11 @@ from django.contrib import admin
 
 urlpatterns = [
     
-    path('NuevaTerapia/', views.NuevaTerapia, name='NuevaTerapia'),
-    path('ListarTerapias/<str:id_paciente>/', views.ListarTerapias, name='ListarTerapias'),
-    path('VerTerapia/<int:terapia_id>/', views.VerTerapia, name='VerTerapia'),
+    # path('NuevaTerapia/', views.NuevaTerapia, name='NuevaTerapia'),
+    path('<str:paciente_id>/HistorialTerapias/NuevaTerapia/', views.NuevaTerapia, name='NuevaTerapia'),
     
+    path('<str:id_paciente>/HistorialTerapias/', views.ListarTerapias, name='ListarTerapias'),
+    
+    # path('VerTerapia/<int:terapia_id>/', views.VerTerapia, name='VerTerapia'),
     
 ]
--- Terapias/views.py
+++ Terapias/views.py
@@ -19,33 +19,43 @@ def ListarTerapias(request, id_paciente):
     return render(request, 'Terapia/HistorialTerapia.html', {'paciente': paciente, 'terapias': terapias})
 
 @login_required
-def NuevaTerapia(request):
+def NuevaTerapia(request,paciente_id):
+    
+    consultas = Consulta.objects.filter(id_paciente=paciente_id).order_by('-consulta_fecha')
+    paciente = get_object_or_404(Paciente,id_paciente=paciente_id)
+    
     if request.method == 'POST':
         # Procesar el formulario enviado
         image_data = request.POST.get('image_data')
         print("Imagen en Base64 recibida:", image_data)
-    
+
+        observacion_terapia = request.POST['observacion_terapia']
+        consulta_terapia= request.POST['consulta_terapia']
+        
         # Guardar los datos en el modelo Terapia vinculándolo a la consulta
         nueva_terapia = Terapia.objects.create(
-            id_consulta = Consulta.objects.get(id_consulta='AO08062301C01'),
-            observacion_terapia= 'holaaaaa',
+            id_consulta = Consulta.objects.get(id_consulta=consulta_terapia),
+            observacion_terapia= observacion_terapia,
             esquema_terapia=image_data
         )
-        nueva_terapia.save()
-        return redirect('ListarTerapias', id_paciente='AO08062301')
+        # nueva_terapia.save()
+        return redirect('ListarTerapias', id_paciente=paciente_id)
 
-    return render(request, 'Terapia/NuevaTerapia.html')
+    return render(request, 'Terapia/NuevaTerapia.html',{
+        'paciente':paciente,
+        'consultas':consultas,
+    })
 
 
-@login_required
-def VerTerapia(request, terapia_id):
-    # Obtener la instancia de Terapia con el ID proporcionado
-    terapia = get_object_or_404(Terapia, pk=terapia_id)
+# @login_required
+# def VerTerapia(request, terapia_id):
+#     # Obtener la instancia de Terapia con el ID proporcionado
+#     terapia = get_object_or_404(Terapia, pk=terapia_id)
 
-    # Decodificar la imagen en Base64 para mostrarla en la plantilla
-    imagen_marcada_base64 = terapia.esquema_terapia
-    context = {
-        'terapia': terapia,
-        'imagen_marcada_base64': imagen_marcada_base64,
-    }
-    return render(request, 'Terapia/DetallesTerapia.html', context)
+#     # Decodificar la imagen en Base64 para mostrarla en la plantilla
+#     imagen_marcada_base64 = terapia.esquema_terapia
+#     context = {
+#         'terapia': terapia,
+#         'imagen_marcada_base64': imagen_marcada_base64,
+#     }
+#     return render(request, 'Terapia/DetallesTerapia.html', context)
