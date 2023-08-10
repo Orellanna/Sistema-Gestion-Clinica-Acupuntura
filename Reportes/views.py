76d101289bed87fac3c8269abb1c29b04a711796
OscarVasquez1 <va17011@ues.edu.sv>
2023-08-09 22:28:44 -0600

    Actualizacion del Modulo de Reportes
    He agregado el grafico de ingreso monetario mensual y un apartado con el total de pagos por paciente.

--- Reportes/views.py
+++ Reportes/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from Pacientes.models import Paciente
 from Pacientes.models import Consulta
+from Pacientes.models import Pago
+from Pacientes.models import Terapia
 from django.db.models import Q
 from django.contrib import messages
 import base64
@@ -17,11 +19,17 @@ def Reportes(request):
 @login_required
 def ListarReportes(request):
     numpacientes = Paciente.objects.filter(deshabilitado=False)
+    datospacientes = Paciente.objects.filter(deshabilitado=False)
     numconsulta = Consulta.objects.filter(deshabilitado=False)
+    numTerapias = Terapia.objects.all()
     total_pacientes = numpacientes.count()
     total_consulta = numconsulta.count()
+    total_terapias = numTerapias.count()
 
-    #calcular rangos de edades
+    total_pago = Pago.objects.all()
+    total = total_pago.aggregate(total=Sum('monto_pago'))['total']
+   
+    #calculo de los rangos de edades
     rangos_edad=[(0,18),(19,35),(36,50),(51,65),(66,100),]
     contador_edades=[]
 
@@ -32,9 +40,14 @@ def ListarReportes(request):
             fechanac_paciente__gt=date.today()- timedelta(days=rango_edad[1]*365),
         ).count()
         contador_edades.append(count)
+   
 
     return render(request, 'Reportes/reportes.html', {
+        'total': total,
+        'pacientes': datospacientes,
         'total_pacientes': total_pacientes,
         'total_consulta': total_consulta,
         'contador_edades': contador_edades,
+        'total_terapias': total_terapias,
     })
+
