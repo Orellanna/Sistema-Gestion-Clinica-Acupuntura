f943a5819aaf458c3c0b185b70d5d7826d8c9236
Alexis Orellana <od18003@ues.edu.sv>
2023-08-10 00:02:07 -0600

    add. ingreso monetario mensual

--- Reportes/views.py
+++ Reportes/views.py
@@ -11,6 +11,8 @@ from django.contrib import messages
 import base64
 from django.db.models import Sum
 import json
+from django.db.models.functions import TruncMonth
+from decimal import Decimal
 
 @login_required
 def Reportes(request):
@@ -18,6 +20,7 @@ def Reportes(request):
 
 @login_required
 def ListarReportes(request):
+    
     numpacientes = Paciente.objects.filter(deshabilitado=False)
     datospacientes = Paciente.objects.filter(deshabilitado=False)
     numconsulta = Consulta.objects.filter(deshabilitado=False)
@@ -28,12 +31,25 @@ def ListarReportes(request):
 
    # Obtener la lista de pacientes con su total de pagos
     listado_pacientes = []
-    
     for paciente in datospacientes:
         total_pago = Pago.objects.filter(id_consulta__id_paciente=paciente.id_paciente).aggregate(total=Sum('monto_pago'))['total']
         paciente.total_pago = total_pago if total_pago else 0
         listado_pacientes.append(paciente)
-   
+
+   # Obtener ingresos por mes
+    ingresos_por_mes = Pago.objects.annotate(month=TruncMonth('fecha_pago')).values('month').annotate(total_ingreso=Sum('monto_pago'))
+    ingresos_mensuales = [0] * 12  # Inicializamos lista con ceros para cada mes
+
+    for ingreso in ingresos_por_mes:
+        mes = ingreso['month'].month - 1  # Los meses en Python comienzan desde 0
+        monto_pago = ingreso['total_ingreso']
+        if monto_pago and monto_pago[0] == '$':
+            total_ingreso = monto_pago[1:].replace(',', '') # Eliminar símbolo "$" y comas
+        else:
+            total_ingreso = monto_pago if monto_pago else 0
+        
+        ingresos_mensuales[mes] = total_ingreso
+    
     #calculo de los rangos de edades
     rangos_edad=[(0,18),(19,35),(36,50),(51,65),(66,100),]
     contador_edades=[]
@@ -53,5 +69,6 @@ def ListarReportes(request):
         'total_consulta': total_consulta,
         'contador_edades': contador_edades,
         'total_terapias': total_terapias,
+        'ingresos_mensuales': ingresos_mensuales
     })
 
