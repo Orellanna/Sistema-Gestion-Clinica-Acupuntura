ddf417542f6b3e0901935a22b68b99dca25d25f8
LF18010 <lf18010@ues.edu.sv>
2023-10-10 00:01:13 -0600

    bugs fix cita, pago

--- Pagos/urls.py
+++ Pagos/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('HistorialPagos/NuevoPago/<str:paciente_id>/', views.NuevoPago, name='NuevoPago'),
     path('HistorialPagos/<str:paciente_id>/', views.ListarPagos, name='ListarPagos'),
-    path('HistorialPagos/<str:paciente_id>/<str:consulta_id>/DetallesPago/<int:pago_id>/', views.DetallesPago, name='DetallesPago'),
-    path('HistorialPagos/<str:paciente_id>/EditarPago/<int:pago_id>/', views.EditarPago, name='EditarPago'),
-    path('HistorialPagos/<str:paciente_id>/EliminarPago/<int:pago_id>/', views.EliminarPago, name='EliminarPago'),
-    path('HistorialPagos/<str:paciente_id>/DetallesPago/<int:pago_id>/imprimir_pago/', views.Imprimir_Pago, name='Imprimir_Pago')
+    path('HistorialPagos/<str:paciente_id>/<str:consulta_id>/DetallesPago/<str:pago_id>/', views.DetallesPago, name='DetallesPago'),
+    path('HistorialPagos/<str:paciente_id>/EditarPago/<str:pago_id>/', views.EditarPago, name='EditarPago'),
+    path('HistorialPagos/<str:paciente_id>/EliminarPago/<str:pago_id>/', views.EliminarPago, name='EliminarPago'),
+    path('HistorialPagos/<str:paciente_id>/DetallesPago/<str:pago_id>/imprimir_pago/', views.Imprimir_Pago, name='Imprimir_Pago')
 
 ]
